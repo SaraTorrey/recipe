ac8a810394afa3741e8a2f3a1061d384f8ee3ac7
Sara Torrey <sara.moravej@gmail.com>
2019-07-30 10:40:45 -0500

    Style recipes page Add new recipe file and route

--- routes/recipes.js
+++ routes/recipes.js
@@ -4,6 +4,8 @@ const Recipe = require("../models/recipe");
 
 router.get("/", (req, res) =>  res.render("recipes/index"));
 
+router.get("/new", (req, res) =>  res.render("recipes/new"));
+
 
 
 module.exports = router;

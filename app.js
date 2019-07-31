d91932651e434082445fbd6a0810cbec8371de4a
Sara Torrey <sara.moravej@gmail.com>
2019-07-30 21:28:02 -0500

    Add login authentication Add register form and route

--- app.js
+++ app.js
@@ -4,15 +4,15 @@ const   express         = require("express"),
         mongoose        = require("mongoose"),
         flash           = require("connect-flash");
         passport        = require("passport");
-        LocalStrategy   = require("passport-local");
+       LocalStrategy   = require("passport-local");
         Recipe          = require("./models/recipe");
         Comment         = require("./models/comment");
         User            = require("./models/user");
         // seedDB          = require("./seeds");
 
-const   commentRoutes    = require("./routes/comments");
+const   commentRoutes   = require("./routes/comments");
         recipesRoutes   = require("./routes/recipes");
-        indexRoutes      = require("./routes/index");
+        indexRoutes     = require("./routes/index");
 
 mongoose.connect( "mongodb://localhost:27017/restful_recipe", {useNewUrlParser: true} );
 
@@ -29,11 +29,11 @@ app.use(require("express-session")({
     saveUninitialized: false
 }));
 
-// app.use(passport.initialize());
-// app.use(passport.session());
-// passport.use(new LocalStrategy(User.authenticate()));
-// passport.serializeUser(User.serializeUser());
-// passport.deserializeUser(User.deserializeUser());
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.use(function(req, res, next){
     res.locals.currentUser = req.user;

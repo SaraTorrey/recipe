e8e67ebe29284758d7bd056b5d096ff7360c0ec1
Sara Torrey <sara.moravej@gmail.com>
2019-08-04 22:10:24 -0500

    Edit new recipe form

--- middleware/index.js
+++ middleware/index.js
@@ -1,2 +1,55 @@
 let Recipe = require("../models/recipe"),
     Comment = require("../models/comment");
+
+let middlewareObj ={};
+
+middlewareObj.checkRecipeOwnership = function (req, res, next) {
+    if (req.isAuthenticated()) {
+        Recipe.findById(req.params.id, function (err, foundRecipe) {
+            if (err) {
+                req.flash("error", "Recipe Not Found!");
+                res.redirect("back");
+            } else {
+                // does user own the campground?
+                if (foundRecipe.author.id.equals(req.user._id)) {
+                    next();
+                } else {
+                    req.flash("error", "Permission Denied!");
+                    res.redirect("back");
+                }
+            }
+        });
+    } else {
+        req.flash("error", "Login First!");
+        res.redirect("back");
+    }
+};
+
+middlewareObj.checkCommentOWnership = function (req, res, next) {
+    if (req.isAuthenticated()) {
+        Recipe.findById(req.params.comment_id, function (err, foundComment) {
+            if (err) {
+                req.redirect("back");
+            } else {
+                if (foundComment.author.id.equals(req.user._id)) {
+                    next();
+                } else {
+                    req.flash("error", "Permission Denied!");
+                    req.redirect("back");
+                }
+            }
+        });
+    } else {
+        res.redirect("back");
+    }
+};
+
+middlewareObj.isLoggedIn = function (req, res, next) {
+    if (req.isAuthenticated()){
+        return(next);
+    }
+    req.flash("error", "Please Login First!");
+    req.flash("/login");
+};
+
+module.exports = middlewareObj;
--- routes/recipes.js
+++ routes/recipes.js
@@ -1,11 +1,47 @@
 const express = require("express");
 const router  = express.Router();
 const Recipe = require("../models/recipe");
+const middleware = require("../middleware");
 
-router.get("/", (req, res) =>  res.render("recipes/index"));
+router.get("/", (req, res) =>
+    Recipe.find({}, function (err, allRecipes) {
+        res.render("recipes/index", {recipes: allRecipes})
+    }));
 
-router.get("/new", (req, res) =>  res.render("recipes/new"));
-router.post("/new", (req, res) => res)
+//Create - add new recipe to DB
+router.post("/", middleware.isLoggedIn, function(req, res) {
+    let name        = req.body.name;
+    let image       = req.body.image;
+    let ingredient1 = req.body.ingredient1;
+    let ingredient2 = req.body.ingredient2;
+    let ingredient3 = req.body.ingredient3;
+    let ingredient4 = req.body.ingredient4;
+    let ingredient5 = req.body.ingredient5;
+    let description = req.body.description;
+    let author      = {
+        id: req.user._id,
+        username: req.user.username
+    };
+    let newRecipe = {
+        name:           name,
+        image:          image,
+        ingredient1:    ingredient1,
+        ingredient2:    ingredient2,
+        ingredient3:    ingredient3,
+        ingredient4:    ingredient4,
+        ingredient5:    ingredient5,
+        description:    description,
+        author:         author
+    };
+    Recipe.create(newRecipe, function (err, newlyCreated) {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log(newlyCreated);
+            res.redirect("/recipes");
+        }
+    });
+});
 
 
 module.exports = router;
